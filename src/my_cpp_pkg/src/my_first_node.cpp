4e727ebb9418f3e9bfce88b8e8f805addda0688f
Hugo Juarez <hsebastianjuarezp@gmail.com>
2026-05-04 15:08:48 -0500

    feat: Modified to use OOP approach of creating a Node

--- src/my_cpp_pkg/src/my_first_node.cpp
+++ src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,10 +1,32 @@
 #include <rclcpp/rclcpp.hpp>
 
+class MyNode : public rclcpp::Node
+{
+public:
+    MyNode() : Node("cpp_test") 
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello world!");
+        _timer = this->create_wall_timer(
+            std::chrono::seconds(1),
+            [this]() {timer_callback();}
+        );
+    }
+
+private:
+    void timer_callback() {
+        RCLCPP_INFO(this->get_logger(), "Hello %d", _counter++);
+    }
+
+    int _counter;
+    rclcpp::TimerBase::SharedPtr _timer;
+
+};
+
 int main(int argc, char ** argv) 
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("cpp_test");
-    RCLCPP_INFO(node->get_logger(), "Hello world");
+    auto node = std::make_shared<MyNode>();
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }
